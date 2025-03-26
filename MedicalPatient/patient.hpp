40ce172b768a7cd88d613da7b7d15c26961a5ad3
Saba-BH <sababahrami202@gmail.com>
2025-03-26 16:51:43 +0430

    Basics definition class

---
+++ MedicalPatient/patient.hpp
@@ -0,0 +1,25 @@
+#ifndef PATIENT_HPP
+#define PATIENT_HPP
+#include <iostream>
+using namespace std;
+class Patient{
+
+    private:
+
+    string Name;
+    float BodyTemperature;
+    int HeartRate;
+    int RespiratoryRate;
+    int BloodPressure;
+    bool check = true ;
+    string encryptInfo(const string &info) const ;
+
+    public:
+    Patient(string , float , int , int , int , bool);
+    void displyInfo() const ;
+    ~Patient();
+
+
+
+};
+#endif
