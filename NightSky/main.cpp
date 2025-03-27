0095edd252dc2c8081c4d5de53196770826a3198
Saba-BH <sababahrami202@gmail.com>
2025-03-27 17:33:11 +0430

    Using the print function

--- NightSky/main.cpp
+++ NightSky/main.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 int main(){
     Sky nightSky(20, 10);
+    Star star1(10 , 5 , 78.1);
+    Star star2(10 , 3 , 66.7);
+    Star star3(2 , 1 , 90.6);
+    Star star4(13 , 8 , 39.3);
+    Star star5(1 , 9 , 100);
+    star1.print();
+    star2.print();
+    star3.print();
+    star4.print();
+    star5.print();
+    nightSky.addStar(star1);
+    nightSky.addStar(star2);
+    nightSky.addStar(star3);
+    nightSky.addStar(star4);
+    nightSky.addStar(star5);
 
-    nightSky.addStar(Star(10,3,66.7));
-    nightSky.addStar(Star(2,1,90.6));
-    nightSky.addStar(Star(13,8,39.3));
-    nightSky.addStar(Star(1,9,100));
-
-    cout << "Night Sky:" << endl;
+    cout <<endl<< "Night Sky:" << endl;
     nightSky.displayStar();
     return 0;
 
